e9f6c93ee77f36aa0a069fe28cc241b3c3f3c725
稲村 創 <so.inamura@gmail.com>
2017-07-01 15:40:44 +0900

    Initial commit

---
+++ Assets/PingPong.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPong : MonoBehaviour
+{
+	const float moveLength = 5;
+
+	float initialZ;
+
+    void Start()
+    {
+		initialZ = transform.position.z;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            initialZ - (moveLength / 2) + Mathf.PingPong(Time.time * 2, moveLength));
+    }
+}
---
+++ Assets/README.md
@@ -0,0 +1,4 @@
+# DepthMask sample
+
+from [DepthMask - Unify Community Wiki](http://wiki.unity3d.com/index.php?title=DepthMask)
+
